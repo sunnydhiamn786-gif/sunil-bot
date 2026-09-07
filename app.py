a4c9f6143f009e810c956f3284ae95982f9fdab8
sunnydhiamn786-gif <sunnydhiamn786@gmail.com>
2026-09-07 11:54:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import streamlit as st
-import google.generativeai as genai
+from google import genai
 
-# --- YAHAN APNI KEY DALO ---
-API_KEY =st.secrets["API_KEY"]
-genai.configure(api_key=API_KEY)
+API_KEY = st.secrets["API_KEY"]
+client = genai.Client(api_key=API_KEY)
 
 st.set_page_config(page_title="Sunil's Bot")
 st.title("🤖 Sunil's Data Analyst Bot")
@@ -19,9 +18,11 @@ if prompt := st.chat_input("Kuch bhi pucho..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
-    model = genai.GenerativeModel("gemini-3.6-flash")
     with st.spinner("Soch raha hu..."):
-        response = model.generate_content(prompt)
+        response = client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents=prompt
+        )
         ans = response.text
 
     st.chat_message("assistant").write(ans)
